7a83b2fa792377dab540e673af29a69ffb1ac1d6
Student <student@admins-air.attlocal.net>
2018-06-03 01:50:09 -0400

    first commit

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
 /* write your script here */
-var database = [
-{
-  name:"Salvador Avelar",
-  born:"December 23, 2001",
-  died:null,
-  school:"Aspire Lionel Wilson Prep",
-  picture:null,
-  bio:"snip"
-},
-{
-  name:"Devin Frisbey",
-  born:"August 27, 2002",
-  dead:null,
-  school:"Aspire Golden State Prep",
-  picture:null,
-  bio:"snip"
-},
-{
-  name:"Orlando Mojica",
-  born:"August 23, 2002",
-  died:null,
-  school:"Aspire Golden State Prep",
-  picture:null,
-  bio:"snip"
-},
-{
-  name:"Edgar Suarez",
-  born:"February 22, 2002",
-  died:null,
-  school:"Aspire Lionel Wilson Prep",
-  picture:null,
-  bio:"snip"
-},
-{
-  name:"Elias Cruz",
-  born:"February 5, 2002",
-  died:null,
-  school:"Oakland Tech",
-  picture:null,
-  bio:"snip"
-}
-];
+var database;
 var searchBar = document.getElementById("search-bar");
 var searchButton = document.getElementById("search-button");
 var autoSuggestions = document.getElementById("auto-suggestions");
 var display = document.getElementById("display");
 
+searchBar.addEventListener("input", getAutoSuggestions);
 searchBar.addEventListener("keypress", checkKey);
 searchButton.addEventListener("click", processInput);
+
+loadData();
+
+function loadData() {
+  searchBar.style.display = "none";
+  searchButton.style.display = "none";
+  fetch("database.json")
+  .then(function(response) {
+    response.json()
+    .then(function(jsonObj) {
+      database = jsonObj;
+      console.log("Database Loaded Successfully");
+    }).then(function() {
+      searchBar.style.display = "block";
+      searchButton.style.display = "block";
+    })
+      });
+    }
+
 
 
 function checkKey(e){
@@ -67,10 +46,10 @@ document.getElementById("search-bar").value= "";
 
 let databaseRecord = getRecord(cleanedInput);
 
-if(databaseRecord == NOT NULL){
+if(databaseRecord != null){
   displayRecord(databaseRecord);
 }else{
-  alert("No results");
+  displaySuggestions(getSuggestions(cleanedInput));
 }
 }
 
@@ -85,7 +64,94 @@ function getRecord(cleanedInput){
 }
 
 function displayRecord(databaseRecord){
+  var recordName = document.createElement("h2");
+  recordName.innerHTML = databaseRecord.name;
+  var recordPicture = document.createElement("img");
+  recordPicture.src = databaseRecord.picture;
+  var recordBorn = document.createElement("p");
+  recordBorn.innerHTML = "<b>Born:</b> " + databaseRecord.born;
+  var recordSchool = document.createElement("p");
+  recordSchool.innerHTML = "<b>School:</b> " + databaseRecord.school;
+  var recordBio = document.createElement("p");
+  recordBio.innerHTML = "<b>Bio:</b> " + databaseRecord.bio;
+  var recordDied = document.createElement("p");
+  if(databaseRecord.died != null) {
+    recordDied.innerHTML = "<b>Died:</b> " + databaseRecord.died;
+  }
+    else {
+      recordDied.innerHTML = "<b>Died:</b> N/A";
+    }
+  display.appendChild(recordName);
+  display.appendChild(recordPicture);
+  display.appendChild(recordBorn);
+  display.appendChild(recordSchool);
+  display.appendChild(recordBio);
+  display.appendChild(recordDied);
 
+}
 
+function getAutoSuggestions(){
+  let cleanedInput = searchBar.value.toLowerCase().trim();
+  document.getElementById("auto-suggestions").innerHTML= "";
+  for(let i = 0; i < database.length; i++){
+    let cleanedRecordName = database[i].name.toLowerCase().trim();
+  if(cleanedRecordName.startsWith(cleanedInput) && cleanedInput.length > 0){
+    let matching = cleanedRecordName.substring(0, searchBar.value.length);
+    let remaining = cleanedRecordName.substring(searchBar.value.length);
+    let result = matching + "<b>" + remaining + "</b>";
+    let button = document.createElement("button");
+    button.innerHTML = result;
+    button.style.display = "block";
+    button.className = "suggestions";
+    activateSuggestionButton(button, database[i]);
+    autoSuggestions.appendChild(button);
+  }
+ }
+if(autoSuggestions.hasChildNodes()){
+  autoSuggestions.style.display = "block";
+}else{
+  autoSuggestions.style.display = "none";
+
+}
+}
+
+function activateSuggestionButton(button, record) {
+  button.addEventListener("click", function() {
+    displayRecord(record);
+    document.getElementById("auto-suggestions").innerHTML = "";
+    document.getElementById("auto-suggestions").style.display = "none";
+    document.getElementById("search-bar").value = "";
+  });
+}
+
+function getSuggestions(cleanedInput){
+  let suggestions = [i];
+  for(let i = 0; i < database.length; i++){
+    let cleanedRecordName = database[i].name.toLowerCase().trim();
+    if(cleanedRecordName.startsWith(cleanedInput) && cleanedRecordName.length > 0){
+      suggestions.push(database[i]);
+    }
+  }
+  return suggestions;
+}
+
+function displaySuggestions(suggestions){
+  document.getElementById("display").innerHTML = "";
+  let paragraph = document.createElement("p");
+  if(suggestion.length > 0){
+    paragraph.innerHTML = "Did you mean:";
+    display.appendChild(paragraph);
+    for(let i = 0; i < database.length; i++){
+      let button = document.createElement("button");
+      button.innerHTML = suggestions[i].name;
+      button.style.display = "block";
+      button.className = "suggestion";
+      activateSuggestionButton("button", suggestions[i]);
+      document.getElementById("display").appendChild(button);
+    }
+  }else{
+    paragraph.innerHTML = "No results!";
+    display.appendChild(paragraph);
+  }
 
 }
